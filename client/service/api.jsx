7342b36cc43bb54c1e5218b7a13edbd40ac518f3
hieu6903 <115693536+chef6903@users.noreply.github.com>
2025-08-30 15:32:58 +0700

    Update api.jsx

--- client/service/api.jsx
+++ client/service/api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL_TODO = "http://localhost:4000/api/todo";
-const API_URL_AUTH = "http://localhost:4000/api/auth";
-const API_URL_USER = "http://localhost:4000/api/user";
+const API_URL_TODO = "https://todoapp-backend-yun2.onrender.com/api/todo";
+const API_URL_AUTH = "https://todoapp-backend-yun2.onrender.com/api/auth";
+const API_URL_USER = "https://todoapp-backend-yun2.onrender.com/api/user";
 
 export const apiTodo = axios.create({
   baseURL: API_URL_TODO,
