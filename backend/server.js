7f8a9c61a3097fa8fe60332022e8519a4fe9d233
hieu6903 <115693536+chef6903@users.noreply.github.com>
2025-08-30 15:55:46 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://todoapp-frontend-yun2.onrender.com"];
 
 app.use(express.json());
 app.use(cookieParser());
